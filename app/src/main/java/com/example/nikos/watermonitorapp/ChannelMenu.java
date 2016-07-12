ef018e74de971088cf6b0703e63ffb36e743a039
NikosVlagoidis <poporov@hotmailcom>
2016-07-12 17:51:28 +0300

    Centre the prompt

--- app/src/main/java/com/example/nikos/watermonitorapp/ChannelMenu.java
+++ app/src/main/java/com/example/nikos/watermonitorapp/ChannelMenu.java
@@ -10,7 +10,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
