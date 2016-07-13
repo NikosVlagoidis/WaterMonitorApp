1f5218b66f6bdaf56ea3546b499942a2428ca5a2
ioantyro <ioantyro@csd.auth.gr>
2016-07-13 13:02:09 +0300

    NewDatabase, Version 2

--- app/src/main/java/com/example/nikos/watermonitorapp/MyDbHelper.java
+++ app/src/main/java/com/example/nikos/watermonitorapp/MyDbHelper.java
@@ -8,8 +8,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDbHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "MyDatabase.db";
+    public static final int DATABASE_VERSION = 2;
+    public static final String DATABASE_NAME = "MyNewDatabase.db";
 
     public MyDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
