4b9cdcbfa8c5d73ab3b3ca144d6578832e7a304b
ioantyro <ioantyro@csd.auth.gr>
2016-07-13 13:06:14 +0300

    New Database - Nickname Added

--- app/src/main/java/com/example/nikos/watermonitorapp/ChannelDatabase.java
+++ app/src/main/java/com/example/nikos/watermonitorapp/ChannelDatabase.java
@@ -12,11 +12,15 @@ public class ChannelDatabase {
     public static abstract class DbEntry implements BaseColumns {
         public static final String TABLE_NAME = "Channel";
         public static final String COLUNM_NAME_ID = "ID";
+        public static final String COLUMN_NAME_NICKNAME = "Nickname";
     }
-        public static final String SQL_CREATE_ENTRIES =
-                "CREATE TABLE " + DbEntry.TABLE_NAME + " (" +
-                        DbEntry._ID + " INTEGER PRIMARY KEY," +
-                        DbEntry.COLUNM_NAME_ID + " INTEGER" + " )";
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + DbEntry.TABLE_NAME + " (" +
+                    DbEntry._ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
+                    DbEntry.COLUMN_NAME_ID + " INTEGER" + COMMA_SEP +
+                    DbEntry.COLUMN_NAME_NICKNAME + TEXT_TYPE + " )";
 
 
     public static String SQL_DELETE_ENTRIES =
